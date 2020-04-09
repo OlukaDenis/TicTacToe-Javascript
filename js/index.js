7c1f8ada73273e9db5afd8ab2c9f45d87880daf9
kalavhan <josue.brigido1@gmail.com>
2020-04-09 13:52:20 -0600

    Add some basic validations

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let player1 = "";
 let player2 = "";
+let lastClick = "";
 const Gameboard = (function () {
   let gameBoard = Array.from(Array(3), () => new Array(3));
 
@@ -13,7 +14,6 @@ const Gameboard = (function () {
 
   const reset = () => {
     gameBoard = Array.from(Array(3), () => new Array(3));
-    console.log(gameBoard);
   };
   return { getBoard, turn, reset };
 })();
@@ -32,16 +32,16 @@ const Player = (name, marker) => {
 };
 
 const Game = (() => {
-  const move = (player1, player2, row, column, cell) => {
+  const move = (row, column, cell) => {
     let tempMarker = "";
-    if (player1.turn == true) {
-      player1.turn = false;
-      player2.turn = true;
-      tempMarker = "X";
+    if (player1.getTurn() === true) {
+      player1.setTurn(false);
+      player2.setTurn(true);
+      tempMarker = player1.getMarker();
     } else {
-      player1.turn = true;
-      player2.turn = false;
-      tempMarker = "0";
+      player1.setTurn(true);
+      player2.setTurn(false);
+      tempMarker = player2.getMarker();
     }
     cell.innerHTML = tempMarker;
     Gameboard.turn(tempMarker, row, column);
@@ -79,8 +79,10 @@ const btnSetPlayers = document.getElementById("setPlayerBtn");
 btnSetPlayers.addEventListener("click", () => {
   const nameOne = document.getElementById("player1");
   const nameTwo = document.getElementById("player2");
-  playerA = Player(nameOne, "X");
-  playerB = Player(nameTwo, "O");
+  player1 = Player(nameOne, "X");
+  player1.setTurn(true);
+  player2 = Player(nameTwo, "0");
+  player2.setTurn(false);
   const boardContainer = document.querySelector("#boardContainer");
   boardContainer.style.display = "flex";
 });
@@ -88,13 +90,16 @@ btnSetPlayers.addEventListener("click", () => {
 document.querySelectorAll(".board-cell").forEach(function (el) {
   let cell = el;
   el.addEventListener("click", function () {
+    if (el.id === lastClick) {
+      return;
+    }
+    lastClick = el.id;
     let row = +this.id.split("|")[0];
     let column = +this.id.split("|")[1];
     if (cell.textContet === "X" || cell.textContent === "0") {
       return;
     }
-    Game.move(playerA, playerB, row, column, cell);
-    console.log(Gameboard.getBoard());
+    Game.move(row, column, cell);
     cell.classList.remove("cursor-pointer");
     cell.classList.add("cursor-not-allowed");
   });
