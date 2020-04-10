4d3300b598ae14402dea09dd11c9c183aa22291a
kalavhan <josue.brigido1@gmail.com>
2020-04-10 11:54:29 -0600

    Fix linter errors

--- js/index.js
+++ js/index.js
@@ -18,32 +18,30 @@ const Gameboard = (function c() {
     gameBoard[row][column] = playerMarker;
   };
   const checkWinner = () => {
-    if (
-      (gameBoard[0][0] != null &&
-        gameBoard[0][0] === gameBoard[0][1]
-        && gameBoard[0][1] === gameBoard[0][2]) ||
-      (gameBoard[1][0] != null &&
-        gameBoard[1][0] === gameBoard[1][1] &&
-        gameBoard[1][1] === gameBoard[1][2]) ||
-      (gameBoard[2][0] != null &&
-        gameBoard[2][0] === gameBoard[2][1] &&
-        gameBoard[2][1] === gameBoard[2][2]) ||
-      (gameBoard[0][0] != null &&
-        gameBoard[0][0] === gameBoard[1][0] &&
-        gameBoard[1][0] === gameBoard[2][0]) ||
-      (gameBoard[0][1] != null &&
-        gameBoard[0][1] === gameBoard[1][1] &&
-        gameBoard[1][1] === gameBoard[2][1]) ||
-      (gameBoard[0][2] != null &&
-        gameBoard[0][2] === gameBoard[1][2] &&
-        gameBoard[1][2] === gameBoard[2][2]) ||
-      (gameBoard[0][0] != null &&
-        gameBoard[0][0] === gameBoard[1][1] &&
-        gameBoard[1][1] === gameBoard[2][2]) ||
-      (gameBoard[0][2] != null &&
-        gameBoard[0][2] === gameBoard[1][1] &&
-        gameBoard[1][1] === gameBoard[2][0])
-    ) {
+    if ((gameBoard[0][0] != null
+      && gameBoard[0][0] === gameBoard[0][1]
+      && gameBoard[0][1] === gameBoard[0][2])
+    || (gameBoard[1][0] != null
+      && gameBoard[1][0] === gameBoard[1][1]
+      && gameBoard[1][1] === gameBoard[1][2])
+    || (gameBoard[2][0] != null
+      && gameBoard[2][0] === gameBoard[2][1]
+      && gameBoard[2][1] === gameBoard[2][2])
+    || (gameBoard[0][0] != null
+      && gameBoard[0][0] === gameBoard[1][0]
+      && gameBoard[1][0] === gameBoard[2][0])
+    || (gameBoard[0][1] != null
+      && gameBoard[0][1] === gameBoard[1][1]
+      && gameBoard[1][1] === gameBoard[2][1])
+    || (gameBoard[0][2] != null
+      && gameBoard[0][2] === gameBoard[1][2]
+      && gameBoard[1][2] === gameBoard[2][2])
+    || (gameBoard[0][0] != null
+      && gameBoard[0][0] === gameBoard[1][1]
+      && gameBoard[1][1] === gameBoard[2][2])
+    || (gameBoard[0][2] != null
+      && gameBoard[0][2] === gameBoard[1][1]
+      && gameBoard[1][1] === gameBoard[2][0])) {
       return true;
     }
     return false;
