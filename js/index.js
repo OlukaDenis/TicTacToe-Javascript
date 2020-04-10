f4bc03c93324050fdc300ecdc0fdfff045cd58c0
kalavhan <josue.brigido1@gmail.com>
2020-04-10 11:55:13 -0600

    Fix linter errors

--- js/index.js
+++ js/index.js
@@ -21,25 +21,25 @@ const Gameboard = (function c() {
     if ((gameBoard[0][0] != null
       && gameBoard[0][0] === gameBoard[0][1]
       && gameBoard[0][1] === gameBoard[0][2])
-    || (gameBoard[1][0] != null
+      || (gameBoard[1][0] != null
       && gameBoard[1][0] === gameBoard[1][1]
       && gameBoard[1][1] === gameBoard[1][2])
-    || (gameBoard[2][0] != null
+      || (gameBoard[2][0] != null
       && gameBoard[2][0] === gameBoard[2][1]
       && gameBoard[2][1] === gameBoard[2][2])
-    || (gameBoard[0][0] != null
+      || (gameBoard[0][0] != null
       && gameBoard[0][0] === gameBoard[1][0]
       && gameBoard[1][0] === gameBoard[2][0])
-    || (gameBoard[0][1] != null
+      || (gameBoard[0][1] != null
       && gameBoard[0][1] === gameBoard[1][1]
       && gameBoard[1][1] === gameBoard[2][1])
-    || (gameBoard[0][2] != null
+      || (gameBoard[0][2] != null
       && gameBoard[0][2] === gameBoard[1][2]
       && gameBoard[1][2] === gameBoard[2][2])
-    || (gameBoard[0][0] != null
+      || (gameBoard[0][0] != null
       && gameBoard[0][0] === gameBoard[1][1]
       && gameBoard[1][1] === gameBoard[2][2])
-    || (gameBoard[0][2] != null
+      || (gameBoard[0][2] != null
       && gameBoard[0][2] === gameBoard[1][1]
       && gameBoard[1][1] === gameBoard[2][0])) {
       return true;
